55a1823bff763b2150f797ce85457281eb6b4ae9
standa2 <stana121@trebesin.cz>
2024-10-09 10:47:10 +0200

    blabla

--- PRG/Python/flask/app.py
+++ PRG/Python/flask/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
@@ -9,6 +9,28 @@ def hello():
 @app.route("/bye")
 def bye():
     return render_template("bye.html")
+
+@app.route("/formular")
+def form():
+    name = request.args.get("name", default="______")
+    input_class = request.args.get("input_class", default="______")
+    message = request.args.get("message", default="______")
+
+    if name != "______" and message != "______"  and input_class != "______" :
+        return redirect(url_for("result",  name = name, input_class = input_class, message = message))
+    
+    return render_template("form.html")
+
+@app.route("/result")
+def result():
+    name = request.args.get("name", default="______")
+    input_class = request.args.get("input_class", default="______")
+    message = request.args.get("message", default="______")
+
+    return render_template("result.html",  name = name, input_class = input_class, message = message)
+    
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
