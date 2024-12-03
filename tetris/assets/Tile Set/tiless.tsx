799c425372909235a55d2e634a8a4416ca8f1027
standa2 <stana121@trebesin.cz>
2024-12-03 23:35:09 +0100

    bleble

--- tetris/assets/Tile Set/tiless.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tilesss" tilewidth="16" tileheight="16" tilecount="60" columns="4">
- <image source="tiles.png" width="64" height="240"/>
-</tileset>
